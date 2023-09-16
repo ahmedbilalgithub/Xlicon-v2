87a0e450df85a74f44801742fe573a75fbd344a8
ahmedbilalgithub <143170431+ahmedbilalgithub@users.noreply.github.com>
2023-09-16 10:53:55 +0500

    Update config.js

--- config.js
+++ config.js
@@ -17,15 +17,15 @@ global.statusseen = true;                 //This is the new variable for control
 
 
 //
-global.Owner = ["8801853262586","8801928502256"];         //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
-global.OwnerNumber = ["8801853262586","8801928502256"];   //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
-global.ownertag = ["8801928502256"];
-global.OwnerName = "Slasher";
+global.Owner = ["923023664501",""];         //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
+global.OwnerNumber = ["923023664501",""];   //If you want singal number so global.Owner = ['8801928502256'] Change into your number.
+global.ownertag = ["923023664501"];
+global.OwnerName = "Ahmed Bilal";
 global.BotName = "XLICON-MD";
 global.packname = "X-2.0 Bot";                             //Do not change.
 global.author = "By: Slasher";                               //Do not change.
 global.BotSourceCode = "https://github.com/ahil15/Xlicon-v2"; //Do not change.
-global.SupportGroupLink = "https://chat.whatsapp.com/EjsQvJNcFGVCSfaBEIxZm2"; 
+global.SupportGroupLink = "https://chat.whatsapp.com/CMO0H8BAFhM2pZLVLhpFW2"; 
 
 
 //
